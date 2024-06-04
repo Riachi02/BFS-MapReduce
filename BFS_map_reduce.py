5ef8c1e672608f2b64eaf988a0312b6f20babfd4
Riachi02 <103325051+Riachi02@users.noreply.github.com>
2024-06-04 15:06:08 +0200

    Add files via upload

--- BFS_map_reduce.py
+++ BFS_map_reduce.py
@@ -1,6 +1,5 @@
 import ray
 import os
-import sys
 import argparse
 from Graph import *
 import time
@@ -10,25 +9,25 @@ class Node:
 
     def __init__(self, id, source):
         self.id = id
-        if self.id == source:
-            self.color = 'GRAY'
+        if self.id == source: 
+            self.color = 'GRAY' # Il nodo sorgente è il primo che viene esplorato
             self.dist = 0
         else:
-            self.color = 'WHITE'
+            self.color = 'WHITE' 
             self.dist = float('inf')
         self.neighbours = []
-        self.path_list = [source]
+        self.path_list = [source] # Tutti i nodi hanno il nodo sorgente all'interno della lista del percorso dal nodo sorgente specificato
 
 
 @ray.remote
 def apply_map(nodes):
-    shuffled_nodes = {}
-    for node in nodes:
-        for id, vertex in map_task(node.id, node):
-            if id not in shuffled_nodes:
+    shuffled_nodes = {} # Struttura dati utilizzata per mantenere i nodi ordinati in base al loro identificativo
+    for node in nodes: # Iterazione sulla lista degli oggetti di tipo Node
+        for id, vertex in map_task(node.id, node): # Shuffling all'interno di ogni worker 
+            if id not in shuffled_nodes: # Ad ogni identificativo (chiave) viene associato un dizionario (valore) che contiene le caratteristiche del nodo
                 shuffled_nodes[id] = {"color": [vertex.color], "dist": [vertex.dist], "neighbours": vertex.neighbours, "path_list": [vertex.path_list]}
             else:
-                shuffled_nodes[id]["color"].append(vertex.color)
+                shuffled_nodes[id]["color"].append(vertex.color) # Se il nodo è già presente nel dizionario, vengono inserite le caratteristiche di questo emesse dalla fase di mapping 
                 shuffled_nodes[id]["dist"].append(vertex.dist)
                 for neighbour in vertex.neighbours:
                     if neighbour not in shuffled_nodes[id]["neighbours"]:
@@ -38,20 +37,20 @@ def apply_map(nodes):
     return shuffled_nodes
 
 def map_task(id, node):
-    results = []
+    results = [] 
     if node.color == "GRAY":
-        for neighbour in node.neighbours:
-            child = Node(neighbour, 0)
-            child.color = "GRAY"
+        for neighbour in node.neighbours: # Iterazione sui vicini del nodo, i quali non sono ancora stati creati come oggetti di tipo Node
+            child = Node(neighbour, 0) # Creazione del nodo
+            child.color = "GRAY" # Aggiornamento dello stato del nodo, in modo tale che venga esplorato alla successiva iterazione
             tmp = []
-            for node_in_path in node.path_list:
+            for node_in_path in node.path_list: # Aggiornamento del percorso dal nodo sorgente, aggiungendo se stesso al percorso del nodo genitore
                 tmp.append(node_in_path)
             tmp.append(neighbour)
             child.path_list = tmp
-            child.dist = node.dist + 1
-            results.append((neighbour, child))
-        node.color = "BLACK"
-        results.append((id, node))
+            child.dist = node.dist + 1 # Incremento della distanza dal nodo sorgente sulla base della distanza mantenuta dal nodo genitore
+            results.append((neighbour, child)) # Emissione della tupla caratterizzata dall'identificativo del nodo e l'oggetto nodo 
+        node.color = "BLACK" # Aggiornamento dello stato del nodo ad 'esplorato'
+        results.append((id, node)) 
     else:
         results.append((id, node))
     return results
@@ -59,46 +58,46 @@ def map_task(id, node):
 @ray.remote
 def reduce_task(results):
     nodes = []
-    for id, values in results.items():
+    for id, values in results.items(): 
         new_node = Node(id, 0)
-        for key, value in values.items():
+        for key, value in values.items(): # Iterazione sulle caratteristiche del nodo
             if key == 'dist':
-                new_node.dist = min(value)
-                index = value.index(new_node.dist)
+                new_node.dist = min(value) # La distanza dal nodo sorgente viene impostata al minimo dall'insieme dei valori ottenuti
+                index = value.index(new_node.dist) # Ne viene individuato l'indice
             elif key == 'color':
-                new_node.color = set_to_darkest(value)
+                new_node.color = set_to_darkest(value) # Il colore viene aggiornato considerando quello più scuro dall'insieme ottenuto
             elif key == 'path_list':
-                new_node.path_list = value[index]
+                new_node.path_list = value[index] # Il percorso dal nodo sorgente viene impostato sulla base dell'indice della distanza minima trovata
             elif key == 'neighbours':
                 new_node.neighbours = value
         nodes.append(new_node)
     return nodes
 
 def set_to_darkest(colors):
-    colori_valori = {"WHITE": 0, "GRAY": 1, "BLACK": 2}
+    colori_valori = {"WHITE": 0, "GRAY": 1, "BLACK": 2} 
     inv_colori = {0: "WHITE", 1: "GRAY", 2: "BLACK"}
-    darkest_color = -1
-    for color in colors:
+    darkest_color = -1 # Variabile di appoggio
+    for color in colors: # Iterazione sulla lista dei colori associati al nodo e ottenuti dalla fase di mapping
         color_value = colori_valori[color]
-        if color_value > darkest_color:
-            darkest_color = color_value
-    return inv_colori[darkest_color]
+        if color_value > darkest_color: 
+            darkest_color = color_value 
+    return inv_colori[darkest_color] # Viene restituito il colore più scuro 
 
 def partition_graph(graph, num_partitions):
     key_list = list(graph.keys())
-    chunk = len(key_list) // num_partitions
+    chunk = len(key_list) // num_partitions # Calcolo della dimensione della singola partizione
     partitions = [
         key_list[i * chunk: (i + 1) * chunk] for i in range(num_partitions)
-    ]
+    ] # Creazione delle partizioni sulla base della dimensione di ciascuno
 
     resto = len(key_list) % num_partitions
-    if resto != 0: # nel caso in cui la divisione dia resto diverso da 0
+    if resto != 0: # Nel caso in cui la divisione dia resto diverso da 0, i nodi rimanenti vengono aggiunti all'ultima partizione
         partitions[len(partitions) - 1].extend(key_list[- resto:])
     graph_partitions = []
 
     for partition in partitions:
         tmp_graph = {}
-        for id in partition:
+        for id in partition: 
             tmp_graph[id] = graph[id]
         graph_partitions.append(tmp_graph)
 
@@ -114,15 +113,15 @@ def main():
     path, num_partitions = args.path, int(args.workers)
 
     start_time = time.time()
-    graph_obj = Graph(path)
-    graph = graph_obj.get_graph()
-    graph_partitions = partition_graph(graph, num_partitions)
-    print(graph)
+    graph_obj = Graph(path) # Creazione dell'oggetto grafo
+    graph = graph_obj.get_graph() 
+    graph_partitions = partition_graph(graph, num_partitions) # Suddivisione del grafo
+
     for graph in graph_partitions:
         nodes = []
-        for key, value in graph.items():
+        for key, value in graph.items(): # Creazione degli oggetti nodo all'interno di tutti i grafi
             node = Node(key, 0)
-            node.neighbours = value
+            node.neighbours = value # Al singolo nodo viene associata la sua lista di vicini
             nodes.append(node)
         new_nodes.append(nodes)
 
@@ -134,14 +133,14 @@ def main():
         shuffled_nodes = {}
 
         # MAP
-        futures = [apply_map.remote(nodes_list) for nodes_list in new_nodes]
+        futures = [apply_map.remote(nodes_list) for nodes_list in new_nodes] 
 
         results = ray.get(futures)
         
         # SHUFFLE
         for result_dict in results:
             for id, vertex in result_dict.items():
-                if id not in shuffled_nodes:
+                if id not in shuffled_nodes: 
                     shuffled_nodes[id] = {"color": vertex["color"], "dist": vertex["dist"], "neighbours": vertex["neighbours"], "path_list": vertex["path_list"]}
                 else:
                     shuffled_nodes[id]["color"].extend(vertex["color"])
@@ -159,36 +158,25 @@ def main():
 
         nodes_res = ray.get(outputs)
         new_nodes, all_nodes = [], []
-        for nodes_list in nodes_res:
+        for nodes_list in nodes_res: # Aggiornamento della lista che mantiene l'insieme dei nodi ad ogni iterazione
             new_nodes.append(nodes_list)
             all_nodes.extend(nodes_list)
 
-        for node in all_nodes:
-            #print(node.id, node.color, node.dist, node.path_list)
-            if node.color == 'BLACK':
-                blacks += 1
-
-        print("Num blacks: ", blacks)
-        if all(node.color == 'BLACK' for node in all_nodes): # exit condition
+        if all(node.color == 'BLACK' for node in all_nodes): # Condizione di uscita 
             break
 
     end_time = time.time()
-    for node_list in nodes_res:
-        for node in node_list:
-            print(node.id, node.color, node.dist, node.path_list)
-
-    # Definire i nomi dei campi
-    fieldnames = ['Dataset', 'n_Workers', 'Tempo']
+    
+    # Gestione del file che memorizza i tempi registrati
+    fieldnames = ['Dataset', 'n_Workers', 'Tempo'] # Intestazioni dei campi
     file_path = 'results.csv'
     file_exist = os.path.isfile(file_path)
     with open(file_path, mode='a', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         
-        # Scrivere l'intestazione solo se il file non esiste
-        if not file_exist:
+        if not file_exist: # Le intestazioni vengono scritte solo se il file non esiste
             writer.writeheader()
         
-        # Scrivere i valori
         writer.writerow({'Dataset': path, 'n_Workers': num_partitions, 'Tempo': end_time - start_time})
     
     print("Total execution time: ", end_time - start_time)
