9f3fa6bcf0f0b8d7f06c93994e017e14e8722cba
daryn04 <104832043+daryn04@users.noreply.github.com>
2024-06-06 10:08:54 +0200

    Add files via upload

--- Node.py
+++ Node.py
@@ -2,13 +2,11 @@ class Node:
 
     def __init__(self, id, source):
         self.id = id
-        if self.id == source:
-            self.color = 'GRAY'
+        if self.id == source: 
+            self.color = 'GRAY' # Il nodo sorgente è il primo che viene esplorato
             self.dist = 0
         else:
-            self.color = 'WHITE'
+            self.color = 'WHITE' 
             self.dist = float('inf')
         self.neighbours = []
-        self.path_list = [source]
-
-        
+        self.path_list = [source] # Tutti i nodi hanno il nodo sorgente all'interno della lista del percorso dal nodo sorgente specificato
